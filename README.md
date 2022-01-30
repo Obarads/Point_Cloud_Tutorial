3e28567156600c50472d5afffc02f1527424b220
obarads <id115561@gmail.com>
2022-01-31 00:37:13 +0900

    update

--- README.md
+++ README.md
@@ -15,7 +15,25 @@ This repository contains tutorial code and supplementary note for point cloud pr
 You can execute most tutorial codes on [Codespaces](https://github.com/features/codespaces)(CPU resource). If you have GPU resource, can execute all tutorial codes. For more environment of CPU and GPU, Please refer to [.devcontainer/README.md](.devcontainer/README.md).
 
 ### 2. About tutorial
-Files of tutorial code is included in python folder. For more tutorial abstraction, Please refer to [python/README.md](python/README.md).
+Tutorial contents are as follows:
+
+| File names                | Abstract                                       | Item                                            |
+| ------------------------- | ---------------------------------------------- | ----------------------------------------------- |
+| basic_code.ipynb          | We introduce basic code used in this tutorial. |                                                 |
+|                           |                                                |                                                 |
+|                           |                                                |                                                 |
+| characteristic.ipynb      | Done (ja)                                      | translation to english                          |
+| competition.ipynb         | To do                                          |                                                 |
+| features.ipynb            | Done (ja)                                      | translation to english and implementation of DL |
+| handcrafted_feature.ipynb | Done (ja)                                      | translation to english                          |
+| nns.ipynb                 | Done (ja)                                      | translation to english                          |
+| normal_estimation.ipynb   | Done (ja)                                      | translation to english and implementation       |
+| registration.ipynb        | To do                                          | add description                                 |
+| downsampling.ipynb        | Done (ja)                                      |                                                 |
+| transformations.ipynb     | Done (ja)                                      | translation to english                          |
+| visualization.ipynb       | Done                                           |                                                 |
+
+
 
 ## About correction
 If you finded any corrections, let us know in Issues.
--- python/tutlibs/nns.py
+++ python/tutlibs/nns.py
@@ -1,10 +1,13 @@
-from typing import Tuple
+import enum
+from typing import List, Tuple
 import numpy as np
 
 from .operator import square_distance
 
 
-def k_nearest_neighbors(coords1: np.ndarray, coords2: np.ndarray, k: int) -> Tuple[np.ndarray, np.ndarray]:
+def k_nearest_neighbors(
+    coords1: np.ndarray, coords2: np.ndarray, k: int
+) -> Tuple[np.ndarray, np.ndarray]:
     """Compute k nearest neighbors between coords1 and coords2.
 
     Args:
@@ -18,18 +21,24 @@ def k_nearest_neighbors(coords1: np.ndarray, coords2: np.ndarray, k: int) -> Tup
     """
 
     # compute distances between coords1 and coords2.
-    point_pairwise_distances = square_distance(coords1, coords2) # shape: (N, M)
+    point_pairwise_distances = square_distance(
+        coords1, coords2
+    )  # ((N, 3), (M, 3)) -> (N, M)
 
     # sort the distances between two points in order of closeness and get top-k indices.
-    idxs = np.argsort(point_pairwise_distances, axis=-1)[:, :k] # shape: (N, k)
+    idxs = np.argsort(point_pairwise_distances, axis=-1)[:, :k]  # (N, M) -> (N, k)
 
     # get the distance between two points according to the top-k indices.
-    square_dists = np.take_along_axis(point_pairwise_distances, idxs, axis=-1) # shape: (N, k)
+    square_dists = np.take_along_axis(
+        point_pairwise_distances, idxs, axis=-1
+    )  # ((N, M), (N, k)) -> (N, k)
 
     return idxs, square_dists
 
 
-def radius_nearest_neighbors(coords1: np.ndarray, coords2: np.ndarray, r: float, k: int = 32) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+def radius_nearest_neighbors(
+    coords1: np.ndarray, coords2: np.ndarray, r: float, k: int = 32
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
     """Compute radius nearest neighbors between coords1 and coords2.
 
     Args:
@@ -48,6 +57,39 @@ def radius_nearest_neighbors(coords1: np.ndarray, coords2: np.ndarray, r: float,
     idxs, square_dists = k_nearest_neighbors(coords1, coords2, k)
 
     # get radius nearest neighbors mask.
-    radius_mask = square_dists < r**2
+    radius_mask = square_dists < r ** 2
 
     return idxs, square_dists, radius_mask
+
+
+# def radius_nearest_neighbors(
+#     coords1: np.ndarray, coords2: np.ndarray, r: float
+# ) -> Tuple[List[np.ndarray], List[np.ndarray]]:
+#     """Compute radius nearest neighbors between coords1 and coords2.
+
+#     Args:
+#         coords1: coordinates of centroid points (N, C)
+#         coords2: coordinates of all points (M, C)
+#         r: radius
+#         k: number of nearest neighbors
+
+#     Returns:
+#         idxs: indices of k nearest neighbors (N, k)
+#         square distances: square distance for kNN (N, k)
+#         mask : radius mask (bool) for idxs an distance (N, k)
+#     """
+
+#     # compute nearest neighbors.
+#     idxs, square_dists = k_nearest_neighbors(coords1, coords2, len(coords2))
+
+#     # get radius nearest neighbors masks.
+#     radius_masks = square_dists < r ** 2
+
+#     # get nearest neighbors according to masks
+#     radius_neighbor_indices = []
+#     radius_neighbor_square_dists = []
+#     for i, radius_mask in enumerate(radius_masks):
+#         radius_neighbor_indices.append(idxs[i, radius_mask])
+#         radius_neighbor_square_dists.append(square_dists[i, radius_mask])
+
+#     return idxs, square_dists
